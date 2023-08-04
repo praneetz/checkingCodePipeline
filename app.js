7e456b4f0053c539f02da46f7e02bd1ce2af57f5
Praneet Negi <praneet@Praneets-MacBook-Air.local>
2023-08-04 18:34:59 +0530

    add buildspec file

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
+app.get("/",(req,res)=>{
+  res.send("Yeah Codepipeline is working good! GREAT")
+})
 app.listen(PORT, () => {
   console.log("App is runnig at=>  " + PORT);
 });
