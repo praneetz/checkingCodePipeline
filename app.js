344d22725371f699b62b0eec0f2e4e27e9399427
Praneet Negi <praneet@Praneets-MacBook-Air.local>
2023-08-04 13:27:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("App is runnig at " + PORT);
+});
